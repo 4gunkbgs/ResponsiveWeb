9ee22f4d60ce3d92ef7270a005fde98f03d4be84
4gunkbgs <satriaagung1111@gmail.com>
2021-02-25 20:26:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <link rel="icon" href="img/1.png" type="image/png">
+
+    <title>ST. Dharma Duta</title>
+  </head>
+  <body>
+    <!-- Navbar --> 
+    <div class="container-fluid bg-primary">
+        <!-- Navbar 1--> 
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark col">
+                <div class="navbar-brand">
+                    <img src="img/1.png" alt="">
+                </div>
+                <div class="navbar-nav mr-auto">
+                    <div class="navbar-brand">
+                        <h3>ST. Dharma Duta</h3>
+                        <h4>Banjar Delod Rurung</h4>
+                    </div>
+                </div>  
+            </nav>                
+        </div>
+                      
+        <!-- Akhir Navbar 1 -->
+
+        <!-- Navbar 2 -->
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light col">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link" href="#">Beranda</a></h5>
+                        </li>
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link" href="#">Anggota</a></h5>
+                        </li>
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link" href="#">Tentang</a></h5>
+                        </li>
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link" href="#">Kontak</a></h5>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav mr-4 ml-auto">
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link text-dark" href="#">Daftar</a></h5>
+                        </li>
+                        <li class="nav-item">
+                            <h5><a class="nav-item nav-link text-dark" href="#">Masuk</a></h5>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </nav>                       
+        </div>
+        <!-- Akhir Navbar 1 -->
+    </div> 
+    <!-- Akhir Container -->
+
+    <!-- Konten -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-8 mt-3 ml-4 bg"> 
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2>Sekaa Gong Anak-Anak</h2>
+                        <hr>
+                        <img src="img/gong1.jpg" alt="" id="img-post">
+                        <p>
+                            Sekaa gong anak-anak merupakan salah satu upaya pelestarian budaya Bali.
+                            Dimana anak-anak muda diajarkan untuk membawakan melodi gamelan tradisional Bali, tentu saja dengan alat tradisional Bali.
+                        </p>
+                        <a class="btn btn-secondary" href="#" role="button">Lihat Selengkapnya...</a>
+                    </div>
+                </div>
+                
+               <br>
+
+               <div class="row">
+                    <div class="col-lg-12">
+                        <h2>Parade Ogoh-Ogoh</h2>
+                        <hr>
+                        <img src="img/gong1.jpg" alt="" id="img-post">
+                        <p>
+                            Parade ogoh-ogoh yang dilaksanakan pada tahun 2019. Acara ini dilaksanakan di Kota Tabanan, seputaran jalan Gajah Mada.
+                            Setiap banjar akan menampilkan atraksi ogoh-ogoh mereka kemudian diarak keliling kota.
+                            Ogoh-ogoh tersebut terdiri dari banyak nama. Sebagian besar diambil dari nama Bhuta Kala dan biasanya berperawakan seram.
+                            Diselenggarakan sebelum hari raya Nyepi, yang bertujuan untuk mengusir makhluk-makhluk jahat yang dapat mengganggu kita selama nyepi, serta menetralisir energi jahat.
+                        </p>
+                        <a class="btn btn-secondary" href="#" role="button">Lihat Selengkapnya...</a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-lg-3">
+                <div class="sidebar">
+                    <div class="row">
+                        <div class="berita col-lg-12">
+                            <div class="sidebar-title">
+                                <h3>Berita Lain</h3>
+                                <hr>
+                            </div>
+                            <ul>
+                                <div class="list-color">
+                                    <li><a href="#">Sekaa Gong Ibu-Ibu</a></li>
+                                    <li><a href="#">Parade Ogoh-Ogoh</a></li>
+                                    <li><a href="#">Lomba 17 Agustus</a></li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-lg-12" style="background-color: #e1e1e1">
+                            <div class="sidebar-title">
+                                <h3>Kegiatan</h3>
+                                <hr>
+                            </div>
+                            <ul>
+                                <div class="list-color">
+                                    <li><a href="#">Gotong Royong</a></li>
+                                    <li><a href="#">HUT ST. Dharma Duta</a></li>
+                                    <li><a href="#">Piodalan</a></li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+     <!-- Akhir Container -->
+    <hr>
+     <!-- footer-->
+    <div class="container-fluid mb-3">
+        <div class="row">
+            <div class="col-lg-8 ml-3 mr-auto">
+                <p>&copy AgungB 2021</p>
+            </div>
+            <div class="sosial col-lg-3">
+                <a href="#"><i class="fab fa-instagram mr-4"></i></a>
+                <a href="#"><i class="fab fa-facebook mr-4"></i></a>
+                <a href="#"><i class="far fa-envelope-open mr-4"></i></a>
+                <a href="#"><i class="fab fa-discord mr-4"></i></a>
+            </div>
+        </div>
+    </div>    
+    <!-- akhir footer-->
+       
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
